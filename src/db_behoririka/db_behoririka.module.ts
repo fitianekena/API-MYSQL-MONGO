1af34819a94d962d4f08344ca8b21ac932a98fd2
fitia <fitia@constellation-group.co>
2023-10-10 17:58:12 +0200

    correction erreur

--- src/db_behoririka/db_behoririka.module.ts
+++ src/db_behoririka/db_behoririka.module.ts
@@ -3,17 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Centre as CentreSql } from './schema/mysql/centre.schema';
 import { Centre, CentreSchema } from './schema/mongodb/centre.schema';
-import { Personnel as PersonnelSql } from './schema/mysql/personnel.schema';
+import { Personnel, Personnel as PersonnelSql } from './schema/mysql/personnel.schema';
 import { Personnel as PersonnelMongo, PersonnelSchema } from './schema/mongodb/personnel.schema';
 import { Fonction as FonctionSql } from './schema/mysql/fonction.schema';
-import { Fonction as FonctionMongo, FonctionSchema } from './schema/mongodb/fonction.schema';
+import { Fonction, Fonction as FonctionMongo, FonctionSchema } from './schema/mongodb/fonction.schema';
 import sequelize from 'sequelize';
 import { SyncroService } from 'src/syncro.service';
 import { MappingService } from 'src/mapping.service';
 import { UtilService } from 'src/util.service';
-import { Visiteaffilie as VisiteaffilieSql } from './schema/mysql/visiteaffilie.schema';
+import { Visiteaffilie, Visiteaffilie as VisiteaffilieSql } from './schema/mysql/visiteaffilie.schema';
 import { Visiteaffilie as VisiteaffilieMongo, VisiteaffilieSchema } from './schema/mongodb/visiteaffilie.schema';
-import { Service as ServiceSql } from './schema/mysql/service.schema';
+import { Service, Service as ServiceSql } from './schema/mysql/service.schema';
 import { Service as ServiceMongo, ServiceSchema } from './schema/mongodb/service.schema';
 import { SyncServicesModule } from 'src/sync-services/sync-services.module';
 import { PersonnelService } from './service/personnel/personnel.service';
@@ -29,22 +29,25 @@ import { VisiteService } from './service/visite/visite.service';
 import { DbBehoririkaService } from './db_behoririka.service';
 import { DbBehoririkaController } from './db_behoririka.controller';
 import { ConfigModule } from '@nestjs/config';
-import { Affilie as AffilieSql } from './schema/mysql/affilie.schema';
+import { Affilie, Affilie as AffilieSql } from './schema/mysql/affilie.schema';
 import { Affilie as AffilieMongoDb, AffilieSchema } from './schema/mongodb/affilie.schema';
 import { AffilieService } from './service/affilie/affilie.service';
 import { AffilieController } from './controllers/affilie/affilie.controller';
-import { Adherent as AdherentSql } from './schema/mysql/adherent.schema';
+import { Adherent, Adherent as AdherentSql } from './schema/mysql/adherent.schema';
 import { Adherent as AdherentMongo, AdherentSchema } from './schema/mongodb/adherent.schema';
 import { AdherentController } from './controllers/adherent/adherent.controller';
 import { AdherentService } from './service/adherent/adherent.service';
-import { MedecinTravail as MedecinTravailSql } from './schema/mysql/medecintravail.schema';
+import { MedecinTravail, MedecinTravail as MedecinTravailSql } from './schema/mysql/medecintravail.schema';
 import { MedecinTravail as MedecinTravailMongo, MedecinTravailSchema } from './schema/mongodb/medecintravail.schema';
 import { MedecinTravailService } from './service/medecintravail/medecintravail.service';
 import { MedecinTravailController } from './controllers/medecintravail/medecintravail.controller';
-import { Ordonnance as OrdonnanceSql } from './schema/mysql/ordonnance.schema';
+import { Ordonnance, Ordonnance as OrdonnanceSql } from './schema/mysql/ordonnance.schema';
 import { Ordonnance as OrdonnanceMongo, OrdonnanceSchema } from './schema/mongodb/ordonnance.schema';
 import { OrdonnanceService } from './service/ordonnance/ordonnance.service';
 import { OrdonnanceController } from './controllers/ordonnance/ordonnance.controller';
+import { AdAffilie, AdAffilieSchema } from 'src/test-mongo/schema/ad-affilie.schema';
+import { BoAffilie, BoAffilieSchema } from 'src/test-mongo/schema/bo-affilie.schema';
+import { DnsAffilie, DnsAffilieSchema } from 'src/test-mongo/schema/dns-affilie.schema';
 
 
 @Module({
@@ -63,15 +66,19 @@ import { OrdonnanceController } from './controllers/ordonnance/ordonnance.contro
       models: [OrdonnanceSql, CentreSql, PersonnelSql, FonctionSql, VisiteaffilieSql, ServiceSql, AffilieSql, AdherentSql, MedecinTravailSql], // Vous pouvez ajuster cette option en fonction de vos besoins
     }),
     MongooseModule.forFeature([
+      { name: DnsAffilie.name, schema: DnsAffilieSchema },
+      { name: AdAffilie.name, schema: AdAffilieSchema },
+      { name: BoAffilie.name, schema: BoAffilieSchema },
+      { name: Affilie.name, schema: AffilieSchema },
       { name: Centre.name, schema: CentreSchema },
-      { name: PersonnelMongo.name, schema: PersonnelSchema },
-      { name: FonctionMongo.name, schema: FonctionSchema },
-      { name: VisiteaffilieMongo.name, schema: VisiteaffilieSchema },
-      { name: ServiceMongo.name, schema: ServiceSchema },
-      { name: AffilieMongoDb.name, schema: AffilieSchema },
-      { name: AdherentMongo.name, schema: AdherentSchema },
-      { name: MedecinTravailMongo.name, schema: MedecinTravailSchema },
-      { name: OrdonnanceMongo.name, schema: OrdonnanceSchema }
+      { name: Adherent.name, schema: AdherentSchema },
+      { name: MedecinTravail.name, schema: MedecinTravailSchema },
+      { name: Ordonnance.name, schema: OrdonnanceSchema },
+      { name: Service.name, schema: ServiceSchema },
+      { name: Personnel.name, schema: PersonnelSchema },
+      { name: Visiteaffilie.name, schema: VisiteaffilieSchema },
+      { name: Fonction.name, schema: FonctionSchema },
+      
     ], 'db_behoririka')],
 
   controllers: [
@@ -137,6 +144,7 @@ import { OrdonnanceController } from './controllers/ordonnance/ordonnance.contro
     CentreService, DbBehoririkaService, MedecinTravailService,
   ],
   exports: [
+    MongooseModule,
     OrdonnanceService,
     MedecinTravailService,
     MongooseModule,
